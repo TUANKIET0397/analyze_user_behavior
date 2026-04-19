f5332a6d15b9c306b13229d786032a5a2d113f1a
TUANKIET0397 <kiet8103@gmail.com>
2026-04-19 17:54:05 +0700

    fix file ignore

--- front-end/src/api/api.js
+++ front-end/src/api/api.js
@@ -1,31 +1,37 @@
-const API_URL = import.meta.env.VITE_API_BACKEND;
+const API_URL = import.meta.env.VITE_API_BACKEND
 
 export const createPrediction = async (payload) => {
-  const res = await fetch(`${API_URL}/api/predictions`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload),
-  });
+    const res = await fetch(`${API_URL}/api/predictions`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+    })
 
-  const data = await res.json();
+    const data = await res.json()
 
-  if (!res.ok) {
-    throw new Error(data?.detail || 'API error');
-  }
+    if (!res.ok) {
+        throw new Error(data?.detail || "API error")
+    }
 
-  return data;
-};
+    return data
+}
 
 export async function explainPrediction(data) {
-  const res = await fetch(`${API_URL}/api/predictions/chart`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
+    const res = await fetch(`${API_URL}/api/predictions/chart`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
 
-  return res.json();
+    const value = await res.json()
+
+    if (!res.ok) {
+        throw new Error(value?.detail || "API error")
+    }
+
+    return value
 }
